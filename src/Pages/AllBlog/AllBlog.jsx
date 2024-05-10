1bac8e29eb5faafaf420734d29bb91aec20a4667
arifraj199 <karif9514@gmail.com>
2024-05-11 02:48:12 +0600

    search functionality error occur

--- src/Pages/AllBlog/AllBlog.jsx
+++ src/Pages/AllBlog/AllBlog.jsx
@@ -1,7 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import AllBlogCard from "./AllBlogCard";
+import axios from "axios";
+import { useContext, useEffect, useRef, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import { CiSearch } from "react-icons/ci";
 
 const AllBlog = () => {
+  const { user } = useContext(AuthContext);
+
+  const textRef = useRef(null);
+
   const {
     data: allBlogs,
     isPending,
@@ -15,6 +23,14 @@ const AllBlog = () => {
     },
   });
 
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    if (allBlogs) {
+      setBlogs(allBlogs);
+    }
+  }, [allBlogs]);
+
   if (isPending) {
     return <span className="loading loading-ring loading-lg"></span>;
   }
@@ -23,13 +39,81 @@ const AllBlog = () => {
     return <p>{error.message}</p>;
   }
 
-  console.log(allBlogs);
+  console.log(blogs);
+
+  const handleWishlist = (blog) => {
+    const { title, image, short_description, long_description, category, _id } =
+      blog;
+    const blogData = {
+      title,
+      image,
+      short_description,
+      long_description,
+      category,
+      userEmail: user?.email,
+      id: _id,
+    };
+    console.log("clicked ", blogData);
+
+    axios
+      .post("http://localhost:5000/wishlist", blogData)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          alert("blog added to the wishlist");
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+
+  
+
+  const handleSearch = () => {
+    const searchText = textRef.current.value;
+    console.log(typeof searchText,searchText);
+
+    axios.get(`http://localhost:5000/blogs?text=${searchText}`)
+    .then(res=>{
+      console.log(res.data)
+    })
+    .catch(err=>{
+      console.log(err.message)
+    })
+  };
+
   return (
     <div className="max-w-screen-xl mx-auto">
-      <h2 className="my-10 text-3xl">All Blogs</h2>
+      <div className="my-20 flex justify-between items-center">
+        <h2 className="text-3xl">All Blogs</h2>
+        <div>
+          <div className="relative flex items-center mt-2">
+            <span className="absolute left-4 top-[14px]">
+              <CiSearch className="text-xl text-gray-400" />
+            </span>
+
+            <input
+              ref={textRef}
+              type="text"
+              placeholder="search by blog title ..."
+              className="block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white border border-gray-200 rounded-lg rounded-r-none pl-11 pr-5 rtl:pr-11 rtl:pl-5 focus:border-blue-400  focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+            <button
+              onClick={handleSearch}
+              className="px-6 py-[11px] font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg rounded-l-none hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 my-12">
         {allBlogs?.map((blog) => (
-          <AllBlogCard key={blog._id} blog={blog}></AllBlogCard>
+          <AllBlogCard
+            key={blog._id}
+            blog={blog}
+            handleWishlist={handleWishlist}
+          ></AllBlogCard>
         ))}
       </div>
     </div>
--- src/Pages/AllBlog/AllBlogCard.jsx
+++ src/Pages/AllBlog/AllBlogCard.jsx
@@ -4,7 +4,7 @@ import { CgDetailsMore } from "react-icons/cg";
 import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-const AllBlogCard = ({ blog }) => {
+const AllBlogCard = ({ blog,handleWishlist }) => {
   const { title, image, short_description, category,_id } = blog;
   return (
     <div className="w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg ">
@@ -36,7 +36,7 @@ const AllBlogCard = ({ blog }) => {
 
             <span className="mx-1">Details</span>
           </Link>
-          <button className="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#4F6F52] rounded-lg hover:bg-[#75A47F] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+          <button onClick={()=>handleWishlist(blog)} className="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#4F6F52] rounded-lg hover:bg-[#75A47F] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
             <svg
               className="w-5 h-5 mx-1"
               xmlns="http://www.w3.org/2000/svg"
