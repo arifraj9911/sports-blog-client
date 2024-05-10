accb8f54621a4217aeebb4c33d8a1956b14e7c8f
arifraj199 <karif9514@gmail.com>
2024-05-10 21:33:36 +0600

    featured blogs react data table implement

--- src/Pages/FeaturedBlog/FeaturedBlog.jsx
+++ src/Pages/FeaturedBlog/FeaturedBlog.jsx
@@ -1,11 +1,66 @@
-
+import { useQuery } from "@tanstack/react-query";
+import "./FeaturedBlogs.css";
+// import { useState } from "react";
+import DataTable from "react-data-table-component";
 
 const FeaturedBlog = () => {
-    return (
-        <div>
-            <h2>Featured blog</h2>
-        </div>
-    );
+  // const [index,setIndex] = useState(0)
+  const { data: featuredBlogs, isPending } = useQuery({
+    queryKey: ["featuredBlogs"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:5000/blogs");
+      // console.log(response)
+      return response.json();
+    },
+  });
+
+  if (isPending) {
+    return <p>loading...</p>;
+  }
+
+  const columns = [
+    {
+      name: "Serial Number",
+      selector: (row, index) => index + 1,
+    },
+    {
+      name: "Blog Title",
+      selector: (row) => row.title,
+      sortable: true,
+    },
+    {
+      name: "Category",
+      selector: (row) => row.category,
+    },
+    {
+      name: "Owner Picture",
+      selector: (row) => <img src={row.image} width="50px" alt="" />,
+    },
+  ];
+
+  const customStyle = {
+    cells: {
+      style: {
+        backgroundColor: "#212121",
+      },
+    },
+  };
+  return (
+    <div style={{}} className="w-5/6 mx-auto">
+      <DataTable
+        title="Featured Blogs"
+        columns={columns}
+        data={featuredBlogs}
+        pagination
+        fixedHeader
+        fixedHeaderScrollHeight="400px"
+        selectableRows
+        selectableRowsHighlight
+        highlightOnHover
+        customStyle={customStyle}
+      ></DataTable>
+    </div>
+  );
 };
 
 export default FeaturedBlog;
--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,11 +1,5 @@
-
-
 const Wishlist = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 };
 
 export default Wishlist;
