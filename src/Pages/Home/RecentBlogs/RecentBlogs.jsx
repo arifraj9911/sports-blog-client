2fa7599692b3eda8a819f241f9d346b0ec23ff93
arifraj199 <karif9514@gmail.com>
2024-05-10 22:39:49 +0600

    wishlist blog added

--- src/Pages/Home/RecentBlogs/RecentBlogs.jsx
+++ src/Pages/Home/RecentBlogs/RecentBlogs.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import BlogsCard from "./BlogsCard";
+import axios from "axios";
 
 const RecentBlogs = () => {
   const { data: blogs,isPending,isError,error } = useQuery({
@@ -16,14 +17,27 @@ const RecentBlogs = () => {
   if(isError){
     return <p>{error.message}</p>
   }
-  console.log(blogs);
+
+  const handleWishlist = (blog)=>{
+    console.log('clicked ',blog);
+    axios.post('http://localhost:5000/wishlist',blog)
+    .then(res=>{
+      console.log(res.data);
+      if(res.data.insertedId){
+        alert('blog added to the wishlist')
+      }
+    })
+    .catch(err=>console.log(err.message))
+  }
+
+  // console.log(blogs);
   return (
     <div className="my-20 flex flex-col items-center">
       <h2 className="text-2xl mb-2">Recent Blogs</h2>
       <p>Progressively network equity invested outside the box thinking.</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 my-12">
         {blogs?.slice(0, 6)?.map((blog) => (
-          <BlogsCard key={blog._id} blog={blog}></BlogsCard>
+          <BlogsCard key={blog._id} blog={blog} handleWishlist={handleWishlist}></BlogsCard>
         ))}
       </div>
     </div>
