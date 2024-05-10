a2b1d69b30d8ebc6405373518b58ca95dee34f5d
arifraj199 <karif9514@gmail.com>
2024-05-10 14:19:05 +0600

    all page data fetching implement

--- src/Pages/AllBlog/AllBlog.jsx
+++ src/Pages/AllBlog/AllBlog.jsx
@@ -1,11 +1,39 @@
-
+import { useQuery } from "@tanstack/react-query";
+import AllBlogCard from "./AllBlogCard";
 
 const AllBlog = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const {
+    data: allBlogs,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["allblogs"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:5000/blogs");
+      return response.json();
+    },
+  });
+
+  if (isPending) {
+    return <span className="loading loading-ring loading-lg"></span>;
+  }
+
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+
+  console.log(allBlogs);
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <h2 className="my-10 text-3xl">All Blogs</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 my-12">
+        {allBlogs?.map((blog) => (
+          <AllBlogCard key={blog._id} blog={blog}></AllBlogCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllBlog;
--- src/Pages/Home/RecentBlogs/RecentBlogs.jsx
+++ src/Pages/Home/RecentBlogs/RecentBlogs.jsx
@@ -2,20 +2,27 @@ import { useQuery } from "@tanstack/react-query";
 import BlogsCard from "./BlogsCard";
 
 const RecentBlogs = () => {
-  const { data: blogs } = useQuery({
+  const { data: blogs,isPending,isError,error } = useQuery({
     queryKey: ["blogs"],
     queryFn: async () => {
       const response = await fetch("http://localhost:5000/blogs");
       return response.json();
     },
   });
+  if(isPending){
+    return <span className="loading loading-ring loading-lg"></span>
+  }
+
+  if(isError){
+    return <p>{error.message}</p>
+  }
   console.log(blogs);
   return (
     <div className="my-20 flex flex-col items-center">
       <h2 className="text-2xl mb-2">Recent Blogs</h2>
       <p>Progressively network equity invested outside the box thinking.</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 my-12">
-        {blogs.slice(0, 6)?.map((blog) => (
+        {blogs?.slice(0, 6)?.map((blog) => (
           <BlogsCard key={blog._id} blog={blog}></BlogsCard>
         ))}
       </div>
