36ef7595d38608c1fb994dec37b18bbc5c126cb6
arifraj199 <karif9514@gmail.com>
2024-05-15 12:24:03 +0600

    scroll top issue solving

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,9 +4,33 @@ import Newsletter from "../Newsletter/Newsletter";
 import RecentBlogs from "../RecentBlogs/RecentBlogs";
 import JoinClub from "../JoinClub/JoinClub";
 import MobileApp from "../MobileApp/MobileApp";
-import ScrollButton from "../../Shared/ScrollButton/ScrollButton";
+// import ScrollButton from "../../Shared/ScrollButton/ScrollButton";
+import { GoArrowUp } from "react-icons/go";
+import { useEffect, useState } from "react";
+import './Home.css'
 
 const Home = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    if (scrollTop > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  useEffect(() => {
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
+
   return (
     <div>
       <Helmet>
@@ -17,7 +41,17 @@ const Home = () => {
       <JoinClub></JoinClub>
       <MobileApp></MobileApp>
       <Newsletter></Newsletter>
-      <ScrollButton></ScrollButton>
+      <button
+      className={`scroll-btn ${
+        isVisible ? "show" : "hide"
+      } flex items-center gap-1`}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
+      <span>Top</span>
+      <span>
+        <GoArrowUp />
+      </span>
+    </button>
     </div>
   );
 };
